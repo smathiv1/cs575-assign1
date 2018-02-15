8001f9b5c51aecb59fc4d3f5b05925fcd149c19b
smathiv1 <smathiv1@q22-33.class.cs.int>
2018-02-14 20:53:17 -0500

    Commit #2: Added basic structure and UI.

--- Project1/CS575_Contacts/src/CS575_Contacts.cpp
+++ Project1/CS575_Contacts/src/CS575_Contacts.cpp
@@ -7,10 +7,18 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 using namespace std;
 
+struct sDirEntry{
+	int iIndex;
+	string sFirstName;
+	string sLastName;
+	string sPhoneNumber;
+};
+using sDirRecord=sDirEntry;
 /*
  * Function Prototype Declarations
  */
@@ -20,11 +28,63 @@ void searchDirectory(char*);
 void deleteEntry(char*);
 void insertEntry(char*, char*);
 
+void createDirectory(char*){
+	/*
+	 * TODO reading input.csv file using inputFileStream.
+	 * As of now, added hard-coded inputs for testing purposes.
+	 *
+	 */
+	string line;
+	ifstream inFile ("input.csv");
+	if (inFile.is_open())
+	{
+		while ( getline (inFile,line) )
+		{
+			cout << line << '\n';
+		}
+		inFile.close();
+	}
+
+	else cout << "Unable to open file";
+}
 int main() {
+	int iOption = 1;
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	cout << "Creating the phone directory.." << endl;
+
+	void createDirectory();
+
+	while(0 != iOption){
+		cout << "Please provide an option" <<endl;
+		cout << "Insert\t\t(press 1)"<<endl;
+		cout << "Search\t\t(press 2)"<<endl;
+		cout << "Delete\t\t(press 3)"<<endl;
+		cout << "Show Directory\t(press 4)"<<endl;
+		cin >> iOption;
+		switch(iOption){
+		case 1:
+			cout << "Insert" << endl;
+			continue;
+		case 2:
+			cout << "Search" << endl;
+			continue;
+		case 3:
+			cout << "Delete" << endl;
+			continue;
+		case 4:
+			cout << "Show Directory" << endl;
+			continue;
+		default:
+			cout << "Enter a valid option! Exiting" <<endl;
+			exit(1);
+		}
+	}
+
 	return 0;
 }
 
+/*
 void createDirectory(char* filePath​){
 	FILE * fp;
 	char * line = NULL;
@@ -61,3 +121,4 @@ void deleteEntry(char* name){
 void insertEntry(char* name, char* phone_number){
 
 }
+*/
